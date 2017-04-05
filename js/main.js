1f5d84041dfe7fe66437cad0aa2aa616cb99736f
Matt Moore <mattmoore@carbonhelix.com>
2017-04-04 20:44:15 -0500

    Fix frustum bugs.

--- js/main.js
+++ js/main.js
@@ -1,49 +1,43 @@
-var container;
-var world;
+var camera;
+var renderer;
+var scene;
 var lights;
-var loader;
+var jsonLoader;
+var textureLoader;
 var dna;
 
 function init() {
-  container = document.querySelector('#container');
-  params = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    view_angle: 45,
-    near: 0.1,
-    far: 10000
-  }
-  lights = loadLights();
-  world = world(params);
+  var fov = 50;
+  var near = 1;
+  var far = 10000;
 
-  $(window).resize(function() {
-    world.renderer.setSize(window.innerWidth, window.innerHeight);
-  });
-}
-
-function world(params) {
   renderer = new THREE.WebGLRenderer();
-  aspect = params.width / params.height;
-  camera = new THREE.PerspectiveCamera(params.width, aspect, params.near, params.far);
+  camera = new THREE.PerspectiveCamera(fov, aspect(), near, far);
+  camera.position.z = 15;
   scene = new THREE.Scene();
   scene.add(camera);
 
+  jsonLoader = new THREE.JSONLoader();
+  textureLoader = new THREE.TextureLoader();
+
   loadMeshes();
-
-  $.each(lights, function(index, item) {
-    scene.add(item);
-  });
-
-  renderer.setSize(params.width, params.height);
-  container.appendChild(renderer.domElement); 
+  loadLights();
 
   requestAnimationFrame(update);
 
-  return {
-    renderer: renderer,
-    camera: camera,
-    scene: scene,
-  };
+  document.querySelector('#container').appendChild(renderer.domElement); 
+  $(window).resize(updateViewport);
+  updateViewport();
+}
+
+function updateViewport() {
+  camera.aspect = aspect();
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function aspect() {
+  return window.innerWidth / window.innerHeight;
 }
 
 function update() {
@@ -63,34 +57,27 @@ function loadMeshes() {
 
 function loadBox() {
   var boxGeometry = new THREE.BoxGeometry(5, 5, 5);
-  var texture = new THREE.ImageUtils.loadTexture('/textures/wood-floor.jpg');
+  var texture = textureLoader.load('/textures/wood-floor.jpg');
   var boxMaterial = new THREE.MeshPhongMaterial({ map: texture, shininess: 1000 });
   box = new THREE.Mesh(boxGeometry, boxMaterial);
   box.position.x = 3;
-  box.position.z = -10;
   scene.add(box);
 }
 
 function loadDNA() {
-  var loader = new THREE.JSONLoader();
-  loader.load('/models/dna/DNA.json', function(geometry, materials) {
+  jsonLoader.load('/models/dna/DNA.json', function(geometry, materials) {
     var material = new THREE.MultiMaterial(materials);
     dna = new THREE.Mesh(geometry, material);
     dna.position.x = -5;
-    dna.position.z = -10;
     dna.rotation.x = -8;
     scene.add(dna);
   });
 }
 
 function loadLights() {
-  lights = [];
-
   pointLight = new THREE.PointLight(0xFFFFFF);
   pointLight.position.x = 30;
   pointLight.position.y = 50;
   pointLight.position.z = 130;
-
-  lights.push(pointLight);
-  return lights;
+  scene.add(pointLight);
 }
